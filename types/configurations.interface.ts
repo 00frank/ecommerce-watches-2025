4706dc614885e47ab00aa6805505aed4e2edf769
00frank <00frankgarcia@gmail.com>
2025-09-17 00:29:40 -0300

    feature: adding configuration type

---
+++ types/configurations.interface.ts
@@ -0,0 +1,15 @@
+interface Configurations {
+    id: number;
+    created_at: string;
+    
+    welcome_message: string;
+    asking_message: string;
+    phone_number: string;
+    email: string;
+    address: string;
+
+    facebook: string;
+    instagram: string;
+}
+
+export default Configurations
--- types/index.ts
+++ types/index.ts
@@ -1,5 +1,6 @@
 import Product from './product.interface'
 import Category from './category.interface'
 import ProductAuditLog from './productAuditLog.interface'
+import Configurations from './configurations.interface'
 
-export type { Product, Category, ProductAuditLog }
+export type { Product, Category, ProductAuditLog, Configurations }
