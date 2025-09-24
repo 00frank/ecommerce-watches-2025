2ce3bcc1cf65189176a4668864290167c17a732b
00frank <00frankgarcia@gmail.com>
2025-09-24 18:41:05 -0300

    using configurations to display custom message

--- app/(main)/layout/Header.layout.tsx
+++ app/(main)/layout/Header.layout.tsx
@@ -1,7 +1,13 @@
-export default function Header() {
-    return (
-        <header className="w-full h-[var(--header-height)] z-[1000] bg-primary-800 flex items-center justify-center ">
-            <p className="text-white text-sm">VENTA MAYORISTA - ENVÍOS A TODO EL PAÍS</p>
-        </header>
-    )
+import { createClient } from "@/utils/supabase/server"
+
+
+export default async function Header() {
+  const supabase = await createClient()
+  const { data: configurations } = await supabase.from('configurations').select('*').single()
+
+  return (
+    <header className="w-full h-[var(--header-height)] z-[1000] bg-primary-800 flex items-center justify-center ">
+      <p className="text-white text-sm">{configurations?.welcome_message}</p>
+    </header>
+  )
 }
