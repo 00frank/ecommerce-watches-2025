dd63c2d7ac9c03ebe957106841b2ed782e2ee2bc
Franco Werner <ifrank44445@gmail.com>
2025-10-02 09:32:28 -0300

    update(productCard):Se mejora el diseño

--- app/(main)/common/components/productCard.tsx
+++ app/(main)/common/components/productCard.tsx
@@ -5,23 +5,37 @@ import Link from "next/link";
 export default function ProductCard({
     name,
     image_url,
-    sku
+    sku,
+    brand
 }: ProductDatabase) {
     return (
-        <article
-            className="h-[350px] max-h-[350px] hover:shadow-lg hover:border-default-300 p-2 rounded-sm overflow-hidden border flex flex-col items-stretch border-default-200">
-            <div className="flex-none overflow-hidden">
-                <img
-                    src={image_url || ""}
-                    className="object-contain rounded-lg w-full h-40"
-                    alt={sku || ""} />
-            </div>
-            <div className="py-3 flex-1 px-4 ">
-                <h3 className="text-default-800 break-words line-clamp-3">{name}</h3>
-            </div>
-            <div className="py-4 flex justify-center">
-                <Link className="font-bold transition-all duration-300 ease-in-out hover:scale-95 text-white bg-primary-700 px-6 p-2 rounded-sm" href={`/productos/${sku}`}>Ver mas</Link>
-            </div>
-        </article>
+        <Link href={`/productos/${sku}`} className="block">
+            <article
+                className="h-[340px] max-h-[340px] cursor-pointer group hover:shadow-lg hover:border-default-300 p-3 rounded-md overflow-hidden border flex flex-col items-stretch border-default-200 transition-shadow duration-300"
+            >
+                <div className="flex-none overflow-hidden rounded-md">
+                    <img
+                        src={image_url || ""}
+                        className="object-contain rounded-md w-full h-40 transition-transform duration-300 ease-in-out group-hover:scale-110"
+                        alt={sku || ""}
+                    />
+                </div>
+                <div className="flex-1  pt-2 text-center ">
+                    <p className="text-[12px] font-semibold tracking-wide text-default-500 uppercase">
+                        {brand}
+                    </p>
+                    <h3 className="text-default-800 uppercase text-[14px] font-medium mt-1 group-hover:underline underline-offset-2 break-words line-clamp-3">
+                        {name}
+                    </h3>
+                </div>
+                <div className="py-3 flex justify-center">
+                    <span className="font-medium text-sm transition-all duration-200 ease-in-out group-hover:bg-primary-700 group-hover:text-white text-primary-700 border border-primary-700 px-6 py-2 rounded-md">
+                        Ver más
+                    </span>
+                </div>
+            </article>
+        </Link>
+
+
     )
 }
