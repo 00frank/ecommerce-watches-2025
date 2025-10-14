b28018250acbe198bbf4e050bf3ab9cade8fd071
00frank <00frankgarcia@gmail.com>
2025-10-14 14:07:12 -0300

    adding product pagination

--- app/admin/productos/page.tsx
+++ app/admin/productos/page.tsx
@@ -3,6 +3,7 @@ import { createClient } from "@/lib/supabase/server"
 import { ProductCard } from './components/ProductCard';
 import { Product } from '@/types';
 import { ProductsSuperInfo } from './components/ProductsSuperInfo';
+import PaginationProducts from './components/Pagination';
 
 // Número de productos por página
 const ITEMS_PER_PAGE = 10;
@@ -52,44 +53,10 @@ export default async function ProductosPage({ searchParams }: { searchParams: Se
           <ProductCard key={product.id} product={product as Product} />
         ))}
       </div>
-      {totalPages > 1 && (
-        <nav className="flex items-center justify-between border-t border-gray-200 px-4 sm:px-0">
-          <div className="-mt-px flex w-0 flex-1">
-            {page > 1 && (
-              <Link
-                href={`/admin/productos?page=${page - 1}`}
-                className="inline-flex items-center border-t-2 border-transparent pr-1 pt-4 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700"
-              >
-                Anterior
-              </Link>
-            )}
-          </div>
-          <div className="hidden md:-mt-px md:flex">
-            {pageNumbers.map((pageNumber) => (
-              <Link
-                key={pageNumber}
-                href={`/admin/productos?page=${pageNumber}`}
-                className={`inline-flex items-center border-t-2 px-4 pt-4 text-sm font-medium ${page === pageNumber
-                  ? 'border-indigo-500 text-indigo-600'
-                  : 'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700'
-                  }`}
-              >
-                {pageNumber}
-              </Link>
-            ))}
-          </div>
-          <div className="-mt-px flex w-0 flex-1 justify-end">
-            {page < totalPages && (
-              <Link
-                href={`/admin/productos?page=${page + 1}`}
-                className="inline-flex items-center border-t-2 border-transparent pl-1 pt-4 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700"
-              >
-                Siguiente
-              </Link>
-            )}
-          </div>
-        </nav>
-      )}
+      <PaginationProducts pagination_info={{
+        current_page: page,
+        total_pages: totalPages
+      }} />
     </div>
   );
 }
