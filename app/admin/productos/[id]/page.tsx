e25bba0f804f78e0f668b1433b97bb22315e0203
00frank <00frankgarcia@gmail.com>
2025-11-05 10:56:47 -0300

    increasing image max size supported

--- app/admin/productos/[id]/page.tsx
+++ app/admin/productos/[id]/page.tsx
@@ -106,8 +106,8 @@ export default function ProductPage() {
 		}
 
 		// Check file size (max 5MB)
-		if (file.size > 5 * 1024 * 1024) {
-			toast.error('La imagen no puede pesar más de 5MB');
+		if (file.size > 10 * 1024 * 1024) {
+			toast.error('La imagen no puede pesar más de 10MB');
 			return;
 		}
 
